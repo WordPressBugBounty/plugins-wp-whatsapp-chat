ff8dbcc43d0b92251ebe28adff0280356fca66d3
Sean Murphy <sean@iamseanmurphy.com>
2026-05-05 15:50:47 +0000

    8.4.6

--- wp-whatsapp-chat/build/new-admin-menu/js/index.asset.php
+++ wp-whatsapp-chat/build/new-admin-menu/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'qlwapp-helpers', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => '40ff28e2e484be4a276e');
+<?php return array('dependencies' => array('lodash', 'qlwapp-helpers', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'dc85ac83af52a1bc6e57');
